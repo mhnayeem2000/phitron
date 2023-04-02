fed262335b332f01860dd65f08d88ca63067f6af
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-02 21:06:37 +0600

    Add_some_functionalities

--- mod2/practice1.c
+++ mod2/practice1.c
@@ -1,12 +1,39 @@
 //Arithmtic Operation
 #include<stdio.h>
 int main(){
-    int n1,n2;
+    int n1,n2,choice;
     int sum = 0 ; 
     printf("Enter Your First Number : ");
     scanf("%d",&n1);
     printf("Enter Your Second Number : ");
     scanf("%d",&n2);
-    sum = n1+n2;
-    printf("Sum of %d and %d is : %d.",n1,n2,sum);
+
+    while (1)
+    {
+        printf("\n\nPlease Choose an Option (1/2/3/4) Which Operation you want To perform : \n1.Summetion \n2.Subtraction \n3.Multiplication \n4.Divide \n\n");
+        printf("Your Choice : ");
+        scanf("%d",&choice);
+        if(choice == 1){
+            sum = n1+n2;
+            printf("Sum of %d and %d is : %d.",n1,n2,sum);
+        }
+        else if(choice == 2){
+            int sub = n1-n2;
+            printf("Subtraction of %d and %d is : %d.",n1,n2,sub);
+        }
+        else if(choice == 3){
+            int mul = n1*n2;
+            printf("Sum of %d and %d is : %d.",n1,n2,mul);
+        }
+        else if(choice == 4){
+            int div = n1/n2;
+            printf("Sum of %d and %d is : %d.",n1,n2,div);
+        }
+        else if(choice > 4){
+            
+            printf("Please Enter Correct Option.");
+        }
+    }
+    
+   
 }
