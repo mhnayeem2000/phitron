cf49f6057305eed009c945078ce775da7699aae1
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-02 20:55:13 +0600

    mod2

---
+++ mod2/practice1.c
@@ -0,0 +1,12 @@
+//Arithmtic Operation
+#include<stdio.h>
+int main(){
+    int n1,n2;
+    int sum = 0 ; 
+    printf("Enter Your First Number : ");
+    scanf("%d",&n1);
+    printf("Enter Your Second Number : ");
+    scanf("%d",&n2);
+    sum = n1+n2;
+    printf("Sum of %d and %d is : %d.",n1,n2,sum);
+}
