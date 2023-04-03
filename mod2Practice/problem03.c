6e23db75a698b50139c4ccdc19c6a20e63c91608
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-03 08:56:44 +0600

    oddEven

---
+++ mod2Practice/problem03.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n; 
+    printf("Enter A positive Value : ");
+    scanf("%d",&n);
+
+    if(n > 0){
+        if(n % 2 == 0){
+            printf("Even Number.");
+        }else
+            printf("Odd Number.");
+    }else{
+        printf("Enter Positive Value.");
+    }
+
+    return 0 ; 
+}
