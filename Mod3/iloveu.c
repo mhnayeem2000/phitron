08feee188dbab0cffbaf8af7f5e9cef28f3b4642
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-04 17:40:35 +0600

    loop

---
+++ Mod3/iloveu.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    printf("Enter the number you want to show : ");
+    scanf("%d",&n);
+    for( i = 1 ; i <= n ; i++){
+        printf("%d. I LOVE YOU.\n",i);
+    }
+}
