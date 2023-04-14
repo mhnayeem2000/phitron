4cf45591744b0306605998c7f7e70f03b1fd6b81
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-14 20:37:03 +0600

    Submit Final Code

--- Assignment 02/ReverseandEven.c
+++ Assignment 02/ReverseandEven.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 int main(){
 
-    
+    int n,arr[100000];
+    scanf("%d",&n);
+    for(int i = 0 ; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
 
+    if( n % 2 == 0){
+        for(int i = n-2 ; i >=0 ; i = i-2){
+        printf("%d ",arr[i]);
+       }
+    }else{
+        for(int i = n-1 ; i >=0 ; i = i-2){
+        printf("%d ",arr[i]);
+    }
     return 0;
+}
 }
