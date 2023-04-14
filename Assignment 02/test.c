8b71ceb7cce8c215c428b8db77ac531beaf0be67
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-14 20:12:41 +0600

    but has prob

---
+++ Assignment 02/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n, arr[100],ver,cngv;
+    scanf("%d",&n);
+    for(int i  = 0 ; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+   scanf("%d %d",&ver,&cngv);
+    for(int i  = 0 ; i < n; i++){
+       arr[ver] = cngv;
+        
+    }
+
+    for(int i  = n-1 ; i >= 0; i--){
+        
+        printf("%d ",arr[i]);
+    }
+  
+     
+    
+
+    return 0 ; 
+}
