ad457ebe0523dec70bde9479696f66694a45f853
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-17 23:35:36 +0600

    esadfds

--- Module 10-String/CF_String_B.c
+++ Module 10-String/CF_String_B.c
@@ -3,12 +3,12 @@
 int main(){
 
 char s[1000001];
-fgets(s,1000001,stdin);
+fgets(s,100000,stdin);
 int i = 0 ; 
 
 while (s[i] != '\\')
 {
-    printf("%c",s);
+    printf("%c",s[i]);
     i++;
 }
 
