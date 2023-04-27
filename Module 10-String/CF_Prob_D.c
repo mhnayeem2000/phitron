8cb45e6a7078166cb9a954a65d7f452a1c4563a3
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-27 16:59:20 +0600

    D solved

--- Module 10-String/CF_Prob_D.c
+++ Module 10-String/CF_Prob_D.c
@@ -2,21 +2,18 @@
 #include<string.h>
 int main(){
 
-    char a[15],b[15];
-    char c[15],d[15];
-    c[15] == a[15];
-    d[15] == b[15];
-    char temp[15];
-
+    char a[15],b[15],c[15];
     scanf("%s %s",a,b);
+    strcpy(c,a);
+
     
+    printf("%d %d\n",strlen(a),strlen(b));
+    printf("%s\n",strcat(c,b));
+    
+        
    char temp = a[0];
     a[0] = b[0];
     b[0] = temp;
-
-    printf("%d %d\n",strlen(a),strlen(b));
-    printf("%s\n",strcat(a,b));
-    
     printf("%s %s\n",a,b);
 
 
