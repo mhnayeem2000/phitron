6c230072ed2b9dceb8c08b6807de31355ab98402
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 02:13:23 +0600

    add

---
+++ CF_Array Problems/B_Searching.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int n, arr[1000000],val;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&val);
+    for(int i=0;i<n;i++){
+        
+    }
+    return 0 ; 
+}
