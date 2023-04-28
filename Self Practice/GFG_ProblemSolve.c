0f347f4c88711bd670ea63fe3baa3daadf9d4145
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 03:01:01 +0600

    not possible at this stage

---
+++ Self Practice/GFG_ProblemSolve.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n*2 ; i++){
+        if(i % 2 != 0){
+           if()
+        }
+    }
+
+    return 0;
+}
