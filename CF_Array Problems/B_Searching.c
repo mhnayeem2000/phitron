802449b7ca7ade0b7022e10ccb144cb99795d7fa
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 03:12:09 +0600

    B complete

--- CF_Array Problems/B_Searching.c
+++ CF_Array Problems/B_Searching.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 int main(){
 
-    int n, arr[1000000],val;
+    int n, arr[100000],val,count = 0;
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     scanf("%d",&val);
     for(int i=0;i<n;i++){
-        
+        if(val == arr[i]){
+            printf("%d",i);
+            count++;
+            break;
+        }
     }
+    if(count == 0){
+        printf("-1");
+    }
+    
     return 0 ; 
 }
