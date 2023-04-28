5c43c26b33fcfc8b2e7e9c1e7a4ab21f980346e2
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 00:31:39 +0600

    adfsa

---
+++ Module 10-String/CF_Prob_M.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    int size , arr[1001],max , min;
+
+    scanf("%d",&size);
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    max = min = arr[0];
+    
+
+    for(int i=0;i<size;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+ //   printf("%d %d",max,min);
+
+    for(int i=0;i<size;i++){
+        if(arr[i] == max){
+            printf("%d ",min);
+        }else if (arr[i] == min)
+        {
+            printf("%d ",max);
+        }
+        else{
+            printf("%d ",arr[i]);
+        }
+        
+    }
+    
+    return 0;
+}
