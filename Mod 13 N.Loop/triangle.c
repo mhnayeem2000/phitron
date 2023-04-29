7684ea36f9e5f759a281e3326fa6bffb514ab9b7
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 23:06:36 +0600

    triangle

---
+++ Mod 13 N.Loop/triangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,s,k=1;
+    scanf("%d",&n);
+    s= n-1;
+    for(i=0;i<n;i++){
+        for(j = s; j >=1; j-- ){
+            printf(" "); 
+        }
+        for(j = 1; j < k; j++){
+            printf(" * ");
+           }
+        printf("\n");
+        k++;
+        s--;
+    }
+    return 0;
+}
