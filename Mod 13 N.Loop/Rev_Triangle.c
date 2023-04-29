e47d55a869c17727cc37e972706310a62fe9fcdd
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 23:46:06 +0600

    triangle

--- Mod 13 N.Loop/Rev_Triangle.c
+++ Mod 13 N.Loop/Rev_Triangle.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n,i,j,s,k=1;
+    int n,i,j,s=0,k;
     scanf("%d",&n);
-    s= n-1;
+    k = 
     for(i=0;i<n;i++){
-        for(j = s; j >=1; j-- ){
+
+        for(j = 1; j <s; j++ ){
             printf(" "); 
         }
-        for(j = 1; j <= k; j++){
-            printf("*");
+        for(j = 1; j < k; j++){
+            printf(" * ");
            }
         printf("\n");
-        k=k+2;
-        s--;
+        k++;
+        s++;
     }
     return 0;
 }
