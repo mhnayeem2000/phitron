c67441102e5450f409da7476036aed79e8aefed7
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-29 21:30:45 +0600

    add comment

--- CF_Array Problems/E_LowestNumber.c
+++ CF_Array Problems/E_LowestNumber.c
@@ -1,3 +1,17 @@
+/*
+Given a number N and an array A of N numbers. Print the lowest number and its position.
+
+Note: if there are more than one answer print first one's position.
+
+Input
+First line contains a number N (2 ≤ N ≤ 1000) number of elements.
+
+Second line contains N numbers (-105  ≤  Ai  ≤  105).
+
+Output
+Print the lowest number and its position (1-index).
+
+*/
 #include<stdio.h>
 int main(){
     int size , arr[10001], min,posi=0;
@@ -16,10 +30,12 @@ int main(){
            posi = i;
         
         }
-         //printf("%d %d",min,i);
         
     }
       printf("%d %d",min,posi+1);
 
     return 0;
 }
+
+
+
