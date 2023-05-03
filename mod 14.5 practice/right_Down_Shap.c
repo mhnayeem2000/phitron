948d09c66e977f969f48d6c551374504665e7aee
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-03 12:43:44 +0600

    Shape

---
+++ mod 14.5 practice/right_Down_Shap.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    int n,s,k;
+    scanf("%d",&n);
+    k = n;
+    s = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = k; j > 0; j--){
+            printf("*");
+        }
+        for(int j = i; j < s; j++){
+            printf(" ");
+        }
+        printf("\n");
+        s++;
+        k--;
+
+    }
+    return 0;
+}
