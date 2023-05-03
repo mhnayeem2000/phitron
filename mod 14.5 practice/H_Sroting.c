9980a1ef94cc2cc08cda4ee50ba7cafc08dc52d1
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-03 12:56:35 +0600

    practice problem

---
+++ mod 14.5 practice/H_Sroting.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n,arr[1000];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
