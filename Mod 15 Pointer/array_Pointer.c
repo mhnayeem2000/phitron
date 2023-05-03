55b7636efdbb2358d71c015658b86e5bc73ce6c2
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-03 15:41:44 +0600

    pinter in array

---
+++ Mod 15 Pointer/array_Pointer.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int arr[5] ={10,20,30,40,50};
+    printf("0th value is %d\n",arr[0]);
+    printf("0th value is %d\n",0[arr]);
+    printf("0th value is %d\n",*(arr));
+    return 0;
+}
