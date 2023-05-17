f034becb0b8d6cdf3908c4f40b6a3cae7abfd77a
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-18 05:18:36 +0600

    ihuih

--- Self Practice/test.c
+++ Self Practice/test.c
@@ -6,14 +6,17 @@ int main()
     int size;
     scanf("%d", &size);
 
-    for(int i = 0; i < size; i++){
-        long long int n1,n2, sum = 0;
+   do{
+    long long int n1,n2, sum = 0;
         scanf("%lld %lld",&n1,&n2);
         for(int i = n1; i <= n2; i++){
             sum = sum + i;
         }
         printf("%lld\n",sum);
-    }    
+   }  
+   while (size);
+   
+   
     
    return 0;
 }
