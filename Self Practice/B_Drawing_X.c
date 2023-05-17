2828b7f0b7abbc92074adde2e133f2223acb7298
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-18 03:39:43 +0600

    Maybe right

--- Self Practice/B_Drawing_X.c
+++ Self Practice/B_Drawing_X.c
@@ -5,21 +5,20 @@ int main(){
     
     for(int i=1; i<=num; i++){
         for(int j=1; j<=num; j++){
-            //int r = 1;
-            int c = num;
-            if(i==j){
+            if( i ==  3 && j == 3) {
+                printf("X");
+            }
+            else if(i==j){
                 printf("\\");
             } 
-            else if ( i == c )
+            else if ( num == i + j-1 )
             {
                 printf("/");
             }
-            
+           
             else {
                 printf("*");
-            }
-            r++;
-            c--;
+            }  
         }
 
         printf("\n");
