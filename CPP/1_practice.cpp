171556e295f661f9ca3e3f89c0e26e038b50893d
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-20 14:24:17 +0600

    add

--- CPP/1_practice.cpp
+++ CPP/1_practice.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 int main(){
-    int num;
-    cout << "Enter number : ";
-    cin >> num;
-    cout << num << endl;
+int num1,num2;
+cout << "Enter Two Number: ";
+cin >> num1 >> num2;
+int sum = num1 + num2;
+int sub = num1 - num2;
+int mul = num1 * num2;
+double div = (float) num1 / num2;
+cout << "sum = " << sum << endl << "Sub = " << sub << endl << "Mul = " << mul << endl << "Div = " << div << endl;
     return 0;
 }
