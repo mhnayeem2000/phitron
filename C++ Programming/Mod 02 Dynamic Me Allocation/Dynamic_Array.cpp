34bec1f0e18f8131e405e7d9991f6d27351307a1
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-24 19:46:01 +0600

    ad

---
+++ C++ Programming/Mod 02 Dynamic Me Allocation/Dynamic_Array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int size;
+    cin >> size;
+    int * arr = new int[size];
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
