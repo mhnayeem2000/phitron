c75cb2ffe83a09f3747c6a811a9cb0500bb82585
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-25 23:18:28 +0600

    add name

--- C++ Programming/Mod 03 Class Obj/constructor.cpp
+++ C++ Programming/Mod 03 Class Obj/constructor.cpp
@@ -5,17 +5,21 @@ class Student{
     int id;
     int roll;
     char section;
+    char name[100];
 
-    Student(int i,int r,int s){
+    Student(int i,int r,char s,char* n){
         id = i;
         roll = r;
         section = s;
+        strcpy(name,n);
     }
 };
 int main(){
-    Student nayeem(1,45,'B');
+    char name[100] = "Nayeem";
+    Student nayeem(1,45,'B',name);
     cout << nayeem.id << endl;
     cout << nayeem.roll << endl;
     cout << nayeem.section << endl;
+    cout << nayeem.name << endl;
     return 0;
 }
