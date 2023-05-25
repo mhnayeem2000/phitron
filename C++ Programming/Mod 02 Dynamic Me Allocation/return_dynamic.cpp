19b6303692a382d45aeeb10e5251422ff2385cb5
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-25 20:08:11 +0600

    dsfsd

---
+++ C++ Programming/Mod 02 Dynamic Me Allocation/return_dynamic.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int * fun(){
+
+        int arr[5];
+        for(int i=0; i<5; i++){
+            cin >> arr[i];
+        }
+        return arr;
+}
+
+int main(){
+    int *b = fun();
+    for(int i=0; i<5; i++){
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
