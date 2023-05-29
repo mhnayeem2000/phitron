5ac56d9f1870e870d50115de46fb258d8f78473c
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-29 12:20:00 +0600

    rfdgd

--- C++ Programming/Mod 06/string_constructor.cpp
+++ C++ Programming/Mod 06/string_constructor.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-
+    string s("Hello world",5);
+    cout << s << endl;
+    string s1 = "NayeemHassan"; 
+    string s2(s1,3);
+    cout << s2 << endl;
     return 0;
 }
