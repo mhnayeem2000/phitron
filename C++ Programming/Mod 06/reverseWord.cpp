6f21d21888de2cd7544d0f658719c273b5f87044
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-29 15:13:22 +0600

    string

---
+++ C++ Programming/Mod 06/reverseWord.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void  print(stringstream& ss){
+    string word;
+    if(ss >> word){
+        cout << word << endl;
+        print(ss);
+    }
+}
+
+int main(){
+   string s; 
+   getline(cin , s);
+   stringstream ss(s);
+   print(ss);
+   return 0;
+}
