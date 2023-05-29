4a8248dbb6a59f8b3b6e9be5b9b42eaa6c1834e6
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-29 15:16:34 +0600

    ad

--- C++ Programming/Mod 06/reverseWord.cpp
+++ C++ Programming/Mod 06/reverseWord.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-void  print(stringstream& ss){
-    string word;
-    if(ss >> word){
-        cout << word << endl;
+void print(stringstream & ss){
+    string woed;
+    if(ss >> woed){
+        cout << woed << endl;
         print(ss);
     }
 }
-
 int main(){
-   string s; 
-   getline(cin , s);
-   stringstream ss(s);
-   print(ss);
-   return 0;
+    string s;
+    getline(cin,s);
+    stringstream ss(s);
+    print(ss);
+    return 0;
 }
