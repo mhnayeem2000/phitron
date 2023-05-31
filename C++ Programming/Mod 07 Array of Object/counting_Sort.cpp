bac8812d20ce538988240adb13136fcd4fd672b4
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-01 05:43:52 +0600

    adsd

--- C++ Programming/Mod 07 Array of Object/counting_Sort.cpp
+++ C++ Programming/Mod 07 Array of Object/counting_Sort.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int size;
-    cin >> size;
+    int n; 
+    cin >> n;
     int frq[26] ={0};
-    for(int i=0; i<size;i++){
+    for(int i = 0 ; i < n ; i++){
         char a;
         cin >> a;
         frq[a-'a']++;
     }
-    for(char i = 'a' ; i < 'z';i++){
-        if(frq[i-'a'] > 0){
-            for(int j = 0; j < frq[i-'a']; j++){
+
+    for(char i = 'a' ; i <= 'z' ; i++){
+        if(frq[i - 'a'] > 0){
+            for(int j = 0 ; j < frq[i - 'a'] ; j++){
                 cout << i;
             }
         }
     }
+
     return 0;
 }
