eb06eee63922210c05ef70a1388c76d9493e89a4
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-05 23:17:51 +0600

    adfasd

--- Basic Data Structure/vec_clear.cpp
+++ Basic Data Structure/vec_clear.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-   
+   vector<int> v;
+   v.push_back(1);
+   v.push_back(2);
+   v.push_back(3);
+   v.clear();
+   for(int i=0; i<v.size(); i++){
+    cout << v[i] << " ";
+   }
+    cout <<  endl;
+    cout << v.size() << endl;
+   cout << v[1] << endl;// clear function memory delete kore na. just value gulo delete kore dey
     return 0 ;
 }
