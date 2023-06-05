6cf04316481d9603bf7ecec7d01fea4ad8e98070
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-05 20:14:59 +0600

    gyg

--- Basic Data Structure/vector.cpp
+++ Basic Data Structure/vector.cpp
@@ -1,11 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> v(10);
-    for(int i=0; i<v.size(); i++){
-        cout << v[i] << " " ;
+   // vector<int> v(5);
+    //vector<int> v(5,2);
+    //vector<int> v1(5,3);
+    //vector<int> v(v1);
+    int arr[5] = {1,3,4,5,6};
+    vector<int> v(arr,arr+5);
+    for(int i = 0 ; i < v.size();i++){
+        cout << v[i] << " ";
     }
     cout << endl;
-
-    return 0;
+    cout << v.size() << endl;
+    return 0 ;
 }
