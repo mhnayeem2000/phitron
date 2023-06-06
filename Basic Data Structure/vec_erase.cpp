ea71e9630472b76d3f93dfaae6c6ec076e746804
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-06 20:25:52 +0600

    erase

--- Basic Data Structure/vec_erase.cpp
+++ Basic Data Structure/vec_erase.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main(){
    vector<int> v = {1,2,3,4,5};
-   v.erase(v.begin()+2);
+   //v.erase(v.begin()+2);
+   //v.erase(v.begin()+2,v.end());
+   v.erase(v.begin()+2,v.begin()+4);
    for(int x : v){
         cout << x << " ";
    }
