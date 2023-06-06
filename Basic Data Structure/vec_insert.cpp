201735b1063fe7d0ebaf0d8411f281566070c193
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-06 22:14:52 +0600

    adf

--- Basic Data Structure/vec_insert.cpp
+++ Basic Data Structure/vec_insert.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> v ={1,2,7,8,9};
-    vector<int> v2 ={3,4,5,6};
-    v.insert(v.begin() +2,v2.begin(),v2.end());
+  vector<int> v={1,2,3,4,5};
+    v.erase(v.begin()+2, v.end());
     for(int x:v){
         cout << x << " ";
     }
