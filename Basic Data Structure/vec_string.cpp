888ca6152d1338316a69d47051c0afa5feac1b7f
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-06 22:54:02 +0600

    add string

--- Basic Data Structure/vec_string.cpp
+++ Basic Data Structure/vec_string.cpp
@@ -1,5 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout << "end" << endl;
+    int n;
+    cin >> n;
+    vector<string> v;
+    for(int i=0; i<n; i++){
+        string s;
+        cin >> s;
+        v.push_back(s);
+    }
+    for(string val : v){
+        cout << val << endl;
+    }
 }
