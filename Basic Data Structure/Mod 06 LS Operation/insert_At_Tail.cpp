a16108aa96ca64c5cfdae1214b5310e26e9e4565
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-12 12:28:37 +0600

    adfas

--- Basic Data Structure/Mod 06 LS Operation/insert_At_Tail.cpp
+++ Basic Data Structure/Mod 06 LS Operation/insert_At_Tail.cpp
@@ -9,10 +9,11 @@ class Node {
         this->next = NULL;
     }
 };
-void inset_At_Tail(Node * &node,int data){
+void inset_At_Tail(Node * &head,int data){
     Node * newNode = new Node(data);
     if(head == NULL){
         head = newNode;
+        return;
     }
     
     Node * temp = head;
@@ -22,21 +23,25 @@ void inset_At_Tail(Node * &node,int data){
     temp->next = newNode;
 }
 void print_linked_List(Node *head){
+    cout << "Your Lined List :";
     Node *temp = head;
     while(temp != NULL){
-        cout << temp->next << " " << endl;
+        cout << temp->data << " ";
         temp = temp->next;
+      
     }
 }
 int main(){
     Node * head = NULL;
     cout << "1. Inset At Tail" << endl;
+    cout << "Select Option : ";
     int op;
     cin >> op;
     if(op == 1){
+        cout << "Please Enter Value :";
         int v;cin >> v;
         inset_At_Tail(head, v);
     }
-
+    print_linked_List(head);
     return 0;
 }
