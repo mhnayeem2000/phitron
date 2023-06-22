f259ddb3840a3936c15382d566dbf1126323f019
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-06-23 01:43:51 +0600

    myList

---
+++ Basic Data Structure/Mod 10 STL/myList_Insert.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    list<int> myList ={10,20,30,40,50};
+    myList.insert(next(myList.begin(),2),100);
+    for(int val : myList){
+        cout << val << endl;
+    }
+}
