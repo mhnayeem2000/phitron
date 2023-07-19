17cad72b3a1afb089f1414185024864e2c00d694
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-07-20 00:05:00 +0600

    nbv

---
+++ Basic Data Structure/Mod 15 practice/bracket.cpp
@@ -0,0 +1,29 @@
+stack <char> st;
+for( char c:s){
+    if( c == '(' && c == '{' && c == '['){
+        st.push(c);
+    }
+    else {
+        if( st.empty() ){
+            return false;
+        }
+        else{
+            if( c ==')' && st.top() == '('){
+                st.pop();
+            }
+            else if (c == '}' && st.top() == '}')
+            {
+                st.pop();
+            }
+            else if ( c == ']' && st.top() == ' ]')
+            {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+return st.empty() ;
