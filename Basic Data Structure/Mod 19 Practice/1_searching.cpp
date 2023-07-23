cbc774f7cecbaa719dfcfcdeb7b515f29fb7b7a9
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-07-24 02:40:23 +0600

    mod 19

---
+++ Basic Data Structure/Mod 19 Practice/1_searching.cpp
@@ -0,0 +1,7 @@
+bool isNodePresent(BinaryTreeNode<int> *root, int x) {
+    if(root == NULL) return false;
+    if(root->data == x) return true;
+    bool l =  isNodePresent(root->left , x);
+    bool r = isNodePresent(root->right , x);
+    return (l || r);
+}
