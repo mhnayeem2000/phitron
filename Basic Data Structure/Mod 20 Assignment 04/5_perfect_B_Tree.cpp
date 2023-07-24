30b3ab3251433485507cab400cc4db03b95980c1
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-07-25 05:02:04 +0600

    all done

---
+++ Basic Data Structure/Mod 20 Assignment 04/5_perfect_B_Tree.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int  val;
+    Node * left;
+    Node * right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* input_tree(){
+    int val;
+    cin >> val;
+    Node * root;
+    if( val == -1) root = NULL;
+    else root = new Node(val);
+    queue<Node*> q;
+    if( root) q.push(root);
+    while(!q.empty()){
+        //1st work
+        Node * p = q.front();
+        q.pop();
+
+        // 2nd work
+        int l, r;
+        cin >> l >> r;
+        Node *  left;
+        Node * right;
+        if( l == -1) left = NULL;
+        else left = new Node(l);
+        if( r == -1) right = NULL;
+        else right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        // 3rd work
+        if( p->left){
+            q.push(p->left);
+        }
+        if( p->right) q.push(p->right);
+
+    }
+    return root;
+}
+int count(Node * root){
+    if( root == NULL ) return 0;
+    int l = count(root->left);
+    int r = count(root->right);
+    return l + r + 1;
+}
+int max_Height(Node * root){
+   if(root == NULL) return 0;
+   int l = max_Height(root->left);
+   int r = max_Height(root->right);
+   return max(l,r)+1;
+}
+int main(){
+    Node * root = input_tree();
+    int mheight =  max_Height(root);
+    int cnodes = count(root);
+   int  calheight = pow(2,mheight)-1;
+    if(cnodes == calheight){
+        cout << "YES" << endl;
+    }else cout << "NO" << endl;
+    return 0;
+}
