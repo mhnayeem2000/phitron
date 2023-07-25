5451fdd1216804260bf9cf7f22e8ed9c9a15e1c3
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-07-25 07:39:02 +0600

    BST

---
+++ Basic Data Structure/Mod 21 BST/search_BST.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int  val;
+    Node * left;
+    Node * right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* input_tree(){
+    int val;
+    cin >> val;
+    Node * root;
+    if( val == -1) root = NULL;
+    else root = new Node(val);
+    queue<Node*> q;
+    if( root) q.push(root);
+    while(!q.empty()){
+        //1st work
+        Node * p = q.front();
+        q.pop();
+
+        // 2nd work
+        int l, r;
+        cin >> l >> r;
+        Node *  left;
+        Node * right;
+        if( l == -1) left = NULL;
+        else left = new Node(l);
+        if( r == -1) right = NULL;
+        else right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        // 3rd work
+        if( p->left){
+            q.push(p->left);
+        }
+        if( p->right) q.push(p->right);
+
+    }
+    return root;
+}
+void level_order(Node * root){
+    if(root == NULL) return;
+     queue <Node*> q;
+     q.push(root);
+     while(!q.empty()) {
+        Node* f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+        if(f->left) q.push(f->left);
+        if(f->right) q.push(f->right);
+     }
+}
+bool search(Node * root , int x){
+    if(root == NULL) return false;
+    if( root->val ==  x) return true;
+
+    if( x < root->val) {
+        return search(root->left,x);   
+    }else {
+        return search(root->right,x);
+    }
+}
+int main(){
+    Node * root = input_tree();
+    if(search(root , 6)){
+        cout << "YES" << endl;
+    }else cout << "NO" << endl;
+    return 0;
+}
