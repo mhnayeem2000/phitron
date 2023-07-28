47e2b55feec384a74856a7c6ce6aaff96f1ace57
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-07-28 20:24:24 +0600

    kjygjg

--- hackerRank Contest/1_triangle.cpp
+++ hackerRank Contest/1_triangle.cpp
@@ -1,16 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-    int a , b , c;
-    cin>> a >> b >> c;
-    if( a >= 0 && b >= 0 && c >= 0){
-        if( (a+b) >c || (a + c ) > b || (b+c)> a){
-            if(  a == b && b == c && a == c){
-                cout << "Yes" << endl;
-                }
-                else  cout << "No" << endl;
-        }
+    int a, b, c;
+    cin >> a >> b >> c;
     
+    if (a >= 0 && b >= 0 && c >= 0) {
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+            if (a == b && b == c && a == c) {
+                cout << "Yes" << endl;
+            } else {
+                cout << "No" << endl;
+            }
+        } else {
+            cout << "No" << endl;
+        }
+    } else {
+        cout << "No" << endl;
     }
+    
     return 0;
 }
