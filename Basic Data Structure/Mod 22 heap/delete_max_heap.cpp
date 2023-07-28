1f5eca5aa597b1ba1d943ead999a64b124f6b873
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-07-28 10:44:32 +0600

    max heap

--- Basic Data Structure/Mod 22 heap/delete_max_heap.cpp
+++ Basic Data Structure/Mod 22 heap/delete_max_heap.cpp
@@ -1,32 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
-void insert_heap(vector<int> &v,int x){
-    v.push_back( x );
-        int cur_indx = v.size() - 1;
-        while( cur_indx != 0 ){
-            int par_indx = (cur_indx-1)/2;
-            if( v[par_indx] < v[cur_indx]){
-                swap( v[par_indx], v[cur_indx] );
-            }else {
-                break;
-            }
-            cur_indx = par_indx;
-        }
-}
-void print_heap(vector<int> &v){
-    for(int val: v){
-        cout << val << " ";
+void insert_heap(vector<int> &v, int x)
+{
+    v.push_back(x);
+    int cur_idx = v.size() - 1;
+    while (cur_idx != 0)
+    {
+        int parent_idx = (cur_idx - 1) / 2;
+        if (v[parent_idx] < v[cur_idx])
+            swap(v[parent_idx], v[cur_idx]);
+        else
+            break;
+        cur_idx = parent_idx;
     }
 }
-int main(){
+void delete_heap(vector<int> &v)
+{
+    v[0] = v[v.size() - 1];
+    v.pop_back();
+    int cur = 0;
+    while (true)
+    {
+        int left_idx = cur * 2 + 1;
+        int right_idx = cur * 2 + 2;
+        int last_idx = v.size() - 1;
+        if (left_idx <= last_idx && right_idx <= last_idx)
+        {
+            // duitai ache
+            if (v[left_idx] >= v[right_idx] && v[left_idx] > v[cur])
+            {
+                swap(v[left_idx], v[cur]);
+                cur = left_idx;
+            }
+            else if (v[right_idx] >= v[left_idx] && v[right_idx] > v[cur])
+            {
+                swap(v[right_idx], v[cur]);
+                cur = right_idx;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else if (left_idx <= last_idx)
+        {
+            // left ase
+            if (v[left_idx] > v[cur])
+            {
+                swap(v[left_idx], v[cur]);
+                cur = left_idx;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else if (right_idx <= last_idx)
+        {
+            // right ase
+            if (v[right_idx] > v[cur])
+            {
+                swap(v[right_idx], v[cur]);
+                cur = right_idx;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+void print_heap(vector<int> v)
+{
+    for (int val : v)
+        cout << val << " ";
+    cout << endl;
+}
+int main()
+{
     int n;
     cin >> n;
     vector<int> v;
-    for( int i = 0; i < n; i++ ){
-        int x ;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
         cin >> x;
-        insert_heap(v,x);
+        insert_heap(v, x);
     }
+    delete_heap(v);
+
     print_heap(v);
+    delete_heap(v);
+    print_heap(v);
+
     return 0;
 }
