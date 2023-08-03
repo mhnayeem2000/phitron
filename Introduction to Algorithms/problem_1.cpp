1921145557bcd708387d6fe7d8e6b3b6e7803772
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-03 23:49:27 +0600

    merge sort

--- Introduction to Algorithms/problem_1.cpp
+++ Introduction to Algorithms/problem_1.cpp
@@ -3,10 +3,33 @@ using namespace std;
 const int N = 1e5+7;
 int nums[N];
 void marge(int l , int mid, int r){
-    int left_size = mid-l+1;
+    int left_size = (mid-l)+1;
     int L[left_size+1];
-    int right_size
-     =  r - (mid+1) -1;
+    int right_size =  r - mid;
+    int R[right_size+1];
+
+    for( int i =l, j = 0 ; i <= mid ; i++,j++){
+        L[j] = nums[i];
+    }
+
+    for( int i = mid+1, j = 0; i <= r; i++ , j++){
+        R[j] = nums[i];
+    }
+
+    L[left_size] = INT_MAX;
+    R[right_size] = INT_MAX;
+
+    int lp = 0 , rp = 0 ;
+    for( int i = l ;   i <= r; i++){
+        if( L[lp] <= R[rp]){
+            nums[i] = L[lp];
+            lp++;
+        }
+        else {
+            nums[i] = R[rp];
+            rp++;
+        }
+    }
 }
 
 void margesort(int l , int r){
@@ -25,7 +48,7 @@ int main(){
         cin >> nums[i];
     }
     
-    sort(nums,nums+n);
+    margesort(0,n-1);
     for( int i = 0 ; i < n ; i++ ){
         cout << nums[i] << " ";
     }
