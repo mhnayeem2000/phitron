e8d7e97fd3e5f2fa217c97dd1f252f2eadb60a15
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-09 14:55:01 +0600

    cycle

---
+++ Algorithms/Mod 7 BFS/cycle_detection.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N =  1e3+5;
+vector<int> adj[N];
+bool visited[N];
+
+bool dfs( int u,int p){
+    bool iscycle = false;
+    visited[u] = true;
+    for( auto v : adj[u]){
+        if( v == p) continue;
+        if( visited[v] ) return true;
+        iscycle = iscycle | dfs(v,u);
+    }
+    return iscycle;
+}
+
+
+
+int main(){
+    int n , m ;
+    cin >> n >> m ; 
+    for( int i = 0 ; i < m ; i++){
+        int u, v;
+        cin >> u >> v ;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    if(dfs(1,-1)){
+        cout << "Has Cycle" << endl;
+    }else cout <<"No Cycle" << endl;
+    return 0;
+}
+
+/*
+ 5 5
+1 2
+2 3
+3 5
+5 4
+4 2                             \Algorit
+Has Cycle
+
+
+5 4
+1 2
+2 3
+3 5
+5 4
+No Cycle
+*/
---
+++ Algorithms/Mod 7 BFS/practice.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3+5;
+vector<int> adj[N];
+bool visited[N];
+
+void dfs(int u){
+    visited[u] = true;
+    cout << "Nodes : " << u << endl;
+    for( auto v : adj[u]){
+        if(visited[v]) continue;
+        dfs(v);
+    }
+}
+
+void bfs( int s){
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        cout << u << endl;
+        for( auto v : adj[u]){
+            if( visited[v] ) continue;
+            q.push(v);
+            visited[v] = true;
+        }
+    }
+}
+
+int main(){
+    int n , m;
+    cin >> n >> m ;
+    for ( int i = 0 ; i < m ; i++){
+        int u , v; 
+        cin >> u >> v ;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    bfs(1);
+    return 0;
+}
