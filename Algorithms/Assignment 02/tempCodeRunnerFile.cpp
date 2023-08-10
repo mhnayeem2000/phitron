f6b79af1b17e68f83f50823a0f8ab3814a3c8d77
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-10 22:59:05 +0600

    hoilo na

---
+++ Algorithms/Assignment 02/tempCodeRunnerFile.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3+5;
+vector<int> adj[N];
+bool visited[N];
+int level[N];
+
+void bfs( int s){
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    level[s] =0;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for( auto v : adj[u]){
+            if( visited[v] ) continue;
+            q.push(v);
+            visited[v] = true;
+            level[v] = level[u] + 1;
+        }
+    }
+}
+
+int main(){
+    int n , m;
+    cin >> n >> m ;
+    for ( int i = 0 ; i < m ; i++){
+        int u , v; 
+        cin >> u >> v ;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int size,f,d;
+    cin >> size ;
+    for( int i = 0 ; i < size ; i++){
+        cin >> f >> d;
+        bfs(f);
+        cout << level[d] << endl;
+    }
+    return 0;
+}
---
+++ Algorithms/Assignment 02/test.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 100000;
+vector<int> adj[N];
+bool visited[N];
+int level[N];
+
+void bfs( int s){
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    level[s] =0;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for( auto v : adj[u]){
+            if( visited[v] ) continue;
+            q.push(v);
+            visited[v] = true;
+            level[v] = level[u] + 1;
+        }
+    }
+}
+
+int main(){
+    int n , m;
+    cin >> n >> m ;
+    for ( int i = 0 ; i < m ; i++){
+        int u , v; 
+        cin >> u >> v ;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int size;
+    cin >> size ;
+    
+    for( int i = 0 ; i < size ; i++){
+        int f,d;
+        cin >> f >> d;
+        for( int j = 0 ; j < n ; j++){
+            visited[j] = false ;
+            level[j] = 0 ; 
+        }
+        bfs(f);
+        if( f == 0 && d == 0 )  cout << 0 << endl;
+        else if( level[d] == 0 ){
+            cout << -1 << endl;
+        } 
+        else cout << level[d] << endl;
+    }
+    
+    return 0;
+}
