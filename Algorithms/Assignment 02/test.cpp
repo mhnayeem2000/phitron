39cc7c1d7b5750bc0b1fb58a1106097919cccc75
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-11 20:56:41 +0600

    fdsfg

--- Algorithms/Assignment 02/test.cpp
+++ Algorithms/Assignment 02/test.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 100000;
+const int N = 1e3+5;
 vector<int> adj[N];
 bool visited[N];
 int level[N];
 
-void bfs(int s) {
+void bfs( int s){
     queue<int> q;
     q.push(s);
     visited[s] = true;
-    level[s] = 0;
-    while (!q.empty()) {
+    level[s] =0;
+    while(!q.empty()){
         int u = q.front();
         q.pop();
-        for (auto v : adj[u]) {
-            if (visited[v]) continue;
+        for( auto v : adj[u]){
+            if( visited[v] ) continue;
             q.push(v);
             visited[v] = true;
             level[v] = level[u] + 1;
@@ -22,38 +22,35 @@ void bfs(int s) {
     }
 }
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
+int main(){
+    int n , m;
+    cin >> n >> m ;
+    for ( int i = 0 ; i < m ; i++){
+        int u , v; 
+        cin >> u >> v ;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-
-    int f;
-    cin >> f;
-    for (int j = 0; j < n; j++) {
-        visited[j] = false;
-        level[j] = 0;
-    }
+    int f,d;
+    cin >> f >> d;
     bfs(f);
-    vector<int> directlyConnected;
-    for (int i = 0; i < n; i++) {
-        if (visited[i]) {
-            directlyConnected.push_back(i);
+    for( int i = 0 ; i < n ; i++){
+        if( level[i] == 2){
+            cout << i <<endl;
         }
     }
-    
-    cout << "Number of houses directly connected to Kamal's house: " << directlyConnected.size() - 1 << endl;
-    cout << "List of 1st level houses: ";
-    for (int i = 0; i < directlyConnected.size(); i++) {
-        if (directlyConnected[i] != f) {
-            cout << directlyConnected[i] << " ";
-        }
-    }
-    cout << endl;
-    
     return 0;
 }
+
+/*
+6 7 
+0 1
+0 2
+1 2
+0 3
+4 2
+3 5
+4 3
+0 5
+
+*/
