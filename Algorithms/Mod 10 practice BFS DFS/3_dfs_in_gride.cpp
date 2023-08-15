281ecc912ac9fcb4477b9c326338ef64cff9349b
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-16 00:34:54 +0600

    incomplete

---
+++ Algorithms/Mod 10 practice BFS DFS/3_dfs_in_gride.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N  1e3+5;
+vector<string> gd;
+int main(){
+    int n ,m ;
+    cin >> n >> m;
+    for(int i = 0 ; i <  n; i++ ){
+        string s;
+        cin >> s;
+        gd.push_back(s);
+    }
+    return 0 ;
+}
