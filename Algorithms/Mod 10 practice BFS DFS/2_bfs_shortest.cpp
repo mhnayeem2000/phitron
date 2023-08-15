f4c7c999f16425db89e7aa03222c3822def02cfe
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-16 00:03:16 +0600

    2

--- Algorithms/Mod 10 practice BFS DFS/2_bfs_shortest.cpp
+++ Algorithms/Mod 10 practice BFS DFS/2_bfs_shortest.cpp
@@ -1,11 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int N = 1e5+10;
+vector<int> g[N];
+bool visited[N];
+vector<int> level(N);
+vector<int> parent(N);
+void bfs(int s){
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    level[s] = 0;
+    parent[s] = -1;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for(int v : g[u]){
+            if( visited[v] ) continue;
+            q.push(v);
+            visited[v] = true;
+            level[v] =  level[u] +1;
+            parent[v] = u;
+        }
+    }
+}
 int main(){
     int n , m ;
     cin >> n >> m;
     for( int i  = 0 ; i < m ; i++){
+        int u , v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    bfs(1);
+    cout << level[n] << " " << endl;
 
-        
+    int cc = n;
+    vector<int> path;
+    while(cc != -1){
+        path.push_back(cc);
+        cc = parent[cc];
+    }
+    reverse(path.begin(), path.end());
+    for( int l  : path){
+        cout << l << " ";
     }
     return  0;
 }
