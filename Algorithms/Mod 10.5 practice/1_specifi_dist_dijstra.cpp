9e5f931e59edf92cca1d277e7c256b6fc952807f
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-19 20:45:19 +0600

    dsadf

---
+++ Algorithms/Mod 10.5 practice/1_specifi_dist_dijstra.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+const int N = 1e3+5;
+const int INF = 1e9+10;
+
+vector<pii> adj[N];
+vector<int> dist(N,INF);
+vector<bool> visited(N);
+
+void dijkstra(int s){
+    priority_queue<pii, vector<pii>,greater<pii>> pq;
+    dist[s] = 0;
+    pq.push({dist[s],s});
+    
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+        visited[u] = true;
+        for(pii vpair: adj[u]){
+            int v = vpair.first;
+            int w = vpair.second;
+            if(visited[v]) continue;
+            if( dist[v] > dist[u]+ w ){
+                dist[v] = dist[u]+ w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
+int main(){
+    int n , m ; 
+    cin >> n >> m; 
+    for( int i = 0; i < m; i++ ){
+        int u , v , w ; 
+        cin >> u >> v >> w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    int start , end;
+    cin >> start >> end;
+    dijkstra(start);
+    cout << dist[end] ;
+    return 0;
+}
+
+/*
+6 9
+1 2 5 
+2 3 2 
+1 3 10
+3 4 1 
+4 1 5 
+1 5 9 
+5 6 1 
+2 6 4 
+2 5 2
+4 6
+*/
