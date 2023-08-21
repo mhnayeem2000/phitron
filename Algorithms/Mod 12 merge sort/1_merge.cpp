7a3dcc27641f818cbd8dac1687f95a4b6a93c7b5
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-21 13:03:41 +0600

    merge sort

---
+++ Algorithms/Mod 12 merge sort/1_merge.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+void merge(int arr[],int l,int m,int r){
+    int leftsize = m-l+1;
+    int rightsize = r-m;
+    int L[leftsize],R[rightsize];
+    int k = 0;
+    for(int i = l ; i <= m ; i++){
+        L[k] = arr[i];
+        k++;
+    }
+    k = 0 ; 
+    for( int j = m+1 ; j <= r ; j++){
+        R[k] = arr[j];
+        k++;
+    }
+
+    int i = 0 , j = 0 ;
+    int curr = l;
+    while( i < leftsize && j < rightsize ){
+        if(L[i] <= R[j]){
+            arr[curr] = L[i];
+            i++;
+        }else {
+            arr[curr] = R[j];
+            j++;
+        }
+        curr++;
+    }
+
+    while(i < leftsize){
+        arr[curr] = L[i];
+        i++;
+        curr++;
+    }
+    while(j < rightsize){
+        arr[curr] = R[j];
+        j++;
+        curr++;
+    }
+
+}
+int main(){
+    int n ;
+    cin >> n;
+    int arr[n];
+    for( int i = 0; i < n; i++ ){
+        cin >> arr[i];
+    }
+    merge(arr,0,3,n-1);
+    for( int i = 0; i < n; i++ ){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+
+/*
+10
+2 3 5 10 1 4 6 8 9 15
+*/
