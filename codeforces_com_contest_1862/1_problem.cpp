883e99761b07ac25c95fbc2563da827744a52052
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-24 22:44:41 +0600

    hvjh

---
+++ codeforces_com_contest_1862/1_problem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int row, cal;
+string s;
+int main(){
+    int sz ;
+    cin >> sz;
+    for( int i = 0; i < sz ; i++ ){  
+        cin >> row >> cal;
+        for(int i = 0; i <row; i++){
+            cin >> s;
+        }
+    }
+    return 0;
+}
