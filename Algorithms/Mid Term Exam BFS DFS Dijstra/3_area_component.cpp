58687121ceca5fa2c34d3ffc7bd64f080f28ca67
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-08-25 01:48:39 +0600

    asdf

--- Algorithms/Mid Term Exam BFS DFS Dijstra/3_area_component.cpp
+++ Algorithms/Mid Term Exam BFS DFS Dijstra/3_area_component.cpp
@@ -1,25 +1,27 @@
 #include<bits/stdc++.h>
 using  namespace std;
+typedef pair<int, int> pii;
 const int N = 1e3+5;
 vector<string> g;
 bool visited[N][N];
 int row, col;
-
+vector<pii> path = {{-1,0},{1,0},{0,-1},{0,1}};
 bool isValied(int i , int j){
     return(i >= 0 && i < row &&  j >= 0 && j <col);
 }
-
+int ct;
 void dfs(int i , int j){
     if(!isValied(i,j)) return;
     if(visited[i][j]) return;
     if(g[i][j] == '-') return;
-
+    ct++;
     visited[i][j] = true;
 
-    dfs(i,j-1);
-    dfs(i,j+1);
-    dfs(i-1,j);
-    dfs(i+1,j);
+    for( auto vpari: path){
+        int ni = vpari.first+i;
+        int nj = j + vpari.second;
+        dfs(ni,nj);
+    }
 }
 
 int main(){
@@ -30,14 +32,19 @@ int main(){
         cin >> x;
         g.push_back(x);
     }
+    int mn = INT_MAX;
     for(int i = 0 ; i<row; i++){
         for(  int j =0 ; j < col ; j++){
+            ct = 0;
             if(g[i][j] == '-') continue;
             if(visited[i][j]) continue;
             dfs(i,j);
+            mn = min(ct,mn);
         }
     }
 
-
+   if( mn == INT_MAX)
+        cout << -1 << endl;
+    else cout << mn << endl;
     return 0;
 }
