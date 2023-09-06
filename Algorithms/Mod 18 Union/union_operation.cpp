b001cc0b8f06e206d639669dd1cf896fbc4513a7
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-06 20:07:08 +0600

    1 down

--- Algorithms/Mod 18 Union/union_operation.cpp
+++ Algorithms/Mod 18 Union/union_operation.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int parent[1000];
-void set( int n){
+int parent[100000];
+int parent_size[100000];
+void dsu_set( int n){
     for( int i=0; i<n; i++){
         parent[i] =  -1;
+        parent_size[i] = 1;
     }
 }
 int find(int node ){
@@ -16,18 +18,31 @@ void ds_union(int a, int b){
     int leaderA = find(a);
     int leaderB = find(b);
     if( leaderA != leaderB ){
-        parent[leaderB] = leaderA;
+        if(parent_size[leaderA] > parent_size[leaderB]){
+            parent[leaderB] = leaderA;
+            parent_size[leaderA] += parent_size[leaderB];
+
+        }else {
+            parent[leaderA] = leaderB;
+            parent_size[leaderB] += parent_size[leaderA];
+        }
     }
  }
 
 int main(){
-    int n,e;
+    int n,e,count = 0;
     cin >> n >> e;
+    dsu_set(n);
     for(int i=0;i<e;i++){
         int a,b;
         cin >> a >> b;
-        ds_union(a,b);
+        int leaderA =  find(a);
+        int leaderB = find(b);
+        if(leaderA == leaderB){
+            count++;
+        }else ds_union(a,b);
     }
+    cout << count << endl;
     return 0;
 }
 
