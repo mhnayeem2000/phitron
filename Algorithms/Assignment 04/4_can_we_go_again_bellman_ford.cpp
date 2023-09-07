f9ac21b8f3a13b24790725d852d22d3877fa736f
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-07 15:55:12 +0600

    a little bit problem

--- Algorithms/Assignment 04/4_can_we_go_again_bellman_ford.cpp
+++ Algorithms/Assignment 04/4_can_we_go_again_bellman_ford.cpp
@@ -7,17 +7,16 @@ vector<pii> g[N];
 int dist[N];
 int n , m;
 bool cycle = false;
-void bellmen( int s){
+void bellman( int s){
     for( int i = 1; i <= n ; i++ ){
         dist[i] = INF;
     }
-    dist[1] = 0;
+    dist[s] = 0;
     for( int i = 1; i < n; i++ ){
         for( int  u = 1; u <= n ; u++ ){
             for( auto vpair : g[u]){
                 int v  = vpair.first;
                 int w = vpair.second;
-
                 if( dist[u] != INF && dist[v] > dist[u]+ w){
                     dist[v] = dist[u]+ w;
                 }
@@ -40,49 +39,31 @@ void bellmen( int s){
 }
 
 
-
-int main(){
+int main() {
     cin >> n >> m;
-    for( int i = 0 ; i < m ; i++ ){
-        int u , v , w;
-        cin >> u >>  v >> w;
-        g[u].push_back({v,w});
+    for(int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
     }
 
-    bellmen(1);
-    if(cycle){
-        cout << "Negative Cycle Detected" << endl;
-    }else {
-        for( int i = 1 ; i <= n ; i++ ){
-            cout << " Distance " << i  ; 
-            cout << " : " << dist[i] << endl;
-        } 
-    }    
+    int source, test;
+    cin >> source;
+    cin >> test;
+    bellman(source);
+    for(int i = 0; i < test; i++) {
+        int dest;
+        cin >> dest;
+
+        if(cycle) {
+            cout << "Negative Cycle Detected" << endl;
+            break;
+        } else if(dist[dest] == INF) {
+            cout << "Not Possible" << endl;
+        } else {
+            cout << dist[dest] << endl;
+        }
+    }
+
     return 0;
 }
-
-
-/*
-case 1:
-7 10
-1 2 6
-1 3 5
-1 4 5
-2 5 -1
-3 2 -2
-3 5 1
-4 3 -2
-4 6 -1
-5 7 3
-4 7 3
-
-
-case 2 :
-
-4 5
-1 3 5
-1 2 4
-2 3 2
-3 4 3
-4 1 -10
-*/
