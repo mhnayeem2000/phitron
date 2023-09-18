757189b4b8900328307a6fcd9eb0e3f11c3c58d2
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-18 16:30:50 +0600

    1 done

---
+++ Algorithms/Assignment 04 New/test.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAX_N = 1005;
+const int MAX_S = 1005;
+int dp[MAX_N][MAX_S];
+
+int knapsack(int n , int s, int v[],int w[]){
+    if( n == 0 || s == 0 ) 
+        return 0;
+    if(dp[n][s] != -1){
+        return dp[n][s];
+    }
+    if(w[n-1] <= s){
+        int opt1 =  knapsack(n-1,s-w[n-1],v,w) + v[n-1];
+        int opt2 = knapsack(n-1,s,v,w);
+        return dp[n][s] = max(opt1,opt2);    
+    }else {
+        return dp[n][s]  = knapsack(n-1,s,v,w);
+    }
+    }
+int main(){
+    int test;
+    cin >> test;
+    while(test--){
+        int n,s;
+        cin >> n >> s;
+        int v[n],w[n];
+
+        for( int i=0;i<n;i++){
+            cin >> w[i];
+        }
+
+        for(int i=0;i<n;i++){
+            cin >> v[i];
+        }
+
+        for(int i=0;i<1000;i++){
+            for( int j=0;j<1000;j++){
+                dp[i][j] = -1;
+            }
+        }
+        cout <<  knapsack(n,s,v,w) << endl;
+        }
+    return 0;
+}
