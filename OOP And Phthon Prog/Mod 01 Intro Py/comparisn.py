c1af7b8b93e30c5215934a5d9335b2ebf8ace200
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-23 12:29:59 +0600

    adf

---
+++ OOP And Phthon Prog/Mod 01 Intro Py/comparisn.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+if(num1 < num2): 
+    print("Num1 is small")
+elif(num2 < num1):
+    print("Num2 is small ")       
+else:    
+    print("Equal numbers")
---
+++ OOP And Phthon Prog/Mod 01 Intro Py/forLoop.py
@@ -0,0 +1,7 @@
+numbers =[1,2,3,4,5,6,7,8,9,10]
+sum = 0 
+for n in numbers:
+    if(n % 2 == 1):
+        sum = sum +n
+
+print(sum)
