b235ff4f266a4655da8c0213d91092000b26e21e
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-24 21:03:50 +0600

    kyf

---
+++ OOP And Phthon Prog/Mod 02/args.py
@@ -0,0 +1,4 @@
+def famous_name(title,fname,*args):
+    name =f'My Title is {title} , My first name is {fname} and My last name is  and is my extra name'
+    return name
+print(famous_name("Student","Mehedi","Nayeem","Hello"))
