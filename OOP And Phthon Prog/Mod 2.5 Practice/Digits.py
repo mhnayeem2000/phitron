c847c3bd5f02a47629b6031015c1b28c2c37e8df
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-25 23:47:40 +0600

    reverse

--- OOP And Phthon Prog/Mod 2.5 Practice/Digits.py
+++ OOP And Phthon Prog/Mod 2.5 Practice/Digits.py
@@ -1,9 +1,7 @@
 test = int(input())
 for x in range(0,test):
-    st = input()
-    l = len(st)
+    s =  input()
     result = ''
-    for i in range(l-1,-1,-1):
-        result += st[i]+' '
-    print(result)
-
+    for i in range(len(s)-1,-1,-1):
+        result += s[i] + ' '
+    print(result)    
