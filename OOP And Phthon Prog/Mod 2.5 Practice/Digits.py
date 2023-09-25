f6a02388b477dcf686e0e32ac7f7618acb3a1384
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-25 23:38:06 +0600

    asd

--- OOP And Phthon Prog/Mod 2.5 Practice/Digits.py
+++ OOP And Phthon Prog/Mod 2.5 Practice/Digits.py
@@ -1,3 +1,8 @@
 test = int(input())
-for i in test:
-    print("Testing")
+for x in range(0,test):
+    st = input()
+    l = len(st)
+    resutl = ''
+    for i in range(l-1,-1,-1):
+        result += st[i]
+print(resutl)
