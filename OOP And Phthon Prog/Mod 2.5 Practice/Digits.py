9407f9af481b9196adaa0a0f31eaa394b6bcfd3c
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-25 23:44:53 +0600

    done

--- OOP And Phthon Prog/Mod 2.5 Practice/Digits.py
+++ OOP And Phthon Prog/Mod 2.5 Practice/Digits.py
@@ -2,7 +2,8 @@ test = int(input())
 for x in range(0,test):
     st = input()
     l = len(st)
-    resutl = ''
+    result = ''
     for i in range(l-1,-1,-1):
-        result += st[i]
-print(resutl)
+        result += st[i]+' '
+    print(result)
+
