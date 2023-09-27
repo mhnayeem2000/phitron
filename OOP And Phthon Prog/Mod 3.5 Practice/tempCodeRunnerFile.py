76f39631f1e8a30b493dbad3c22762dfc3dd2b3b
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-28 04:39:58 +0600

    practice problem

---
+++ OOP And Phthon Prog/Mod 3.5 Practice/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+test = int(input())
+for i in  range(0,test):
+    n = input()
+    lst = n.split()
+    rev = list(reversed(lst))
+    if lst == rev :
+        print("YES")
+    else : print("NO")    
