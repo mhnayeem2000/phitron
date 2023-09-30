8aa98b29107a24153177459775be722de811f16c
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-09-30 22:04:10 +0600

    sdfas

--- CF Problem Contest/div-4/1829_A.cpp
+++ CF Problem Contest/div-4/1829_A.cpp
@@ -1,23 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s;
-    cin >> s;
-    int count = 0;
-    string m[10] ="codeforces";
-    /*
-    
-    
-    for(int i=0; i<s.length()+1; i++){
-        if(s[i] != m[i]){
-            count++;
+    int test;
+    cin >> test;
+    while(test--){
+        string s;
+        cin >> s;
+        int count = 0;
+        string m ="codeforces";
+        
+        for(int i=0; i<s.length()+1; i++){
+            if(s[i] != m[i]){
+                count++;
+            }
         }
+        cout << count << endl;
     }
-    */
-    for(int i=0; i<10; i++){
-        cout << m << endl;
-    }
-    cout << count << endl;
-    cout << m[1] << endl;
     return 0;
 }
