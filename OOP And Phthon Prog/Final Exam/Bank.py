817774041801361192b647598240a73fe28665a1
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-10-15 01:16:37 +0600

    ads

---
+++ OOP And Phthon Prog/Final Exam/Bank.py
@@ -0,0 +1,9 @@
+class Info:
+    def __init__(self,name):
+        self.name = name
+        print(f"Hei {self.name} Welcome to My Bank.com")
+        print("1 .I have Already an Account")
+        print("2 .I am new Here")
+
+nayeem = Info("Nayeem")
+
