33939c87dacf929b511a4ca27ae8ce7f6c6bfc03
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-10-22 09:17:47 +0600

    8y

---
+++ CF Problem Contest/978/A_remove_duplicates.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin >> num;
+    vector<int> vt;
+    int arr[num];
+    for(int i = 0; i < num;i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < num;i++){
+        for( int j = 0; j < num; j++){
+            if( arr[i] != arr[j] ){
+
+            }
+        }
+    }
+    return 0;
+}
