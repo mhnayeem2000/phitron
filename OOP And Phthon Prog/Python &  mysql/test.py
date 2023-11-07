a02dd5e905f319f89d238dc89421b8aa5d5bb8b1
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-11-07 06:09:59 +0600

    asd

--- OOP And Phthon Prog/Python &  mysql/test.py
+++ OOP And Phthon Prog/Python &  mysql/test.py
@@ -1,5 +1,13 @@
 import mysql.connector
-mydb =  mysql.connector.connection{
 
-    
+# Create a dictionary with the connection parameters
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "passwd": "aminayeem..1"
 }
+
+# Create a MySQL connection using the configuration
+mydb = mysql.connector.connect(**db_config)
+
+print(mydb)
